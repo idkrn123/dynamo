f55b7da09da2dc1bef090ee4f617ce008699c9c4
Dylan Hart <god@linux.com>
2023-06-19 15:48:38 -0500

    Update chat_utils.py

--- chat_utils.py
+++ chat_utils.py
@@ -1,29 +1,28 @@
 import os
 import requests
 from tenacity import retry, wait_random_exponential, stop_after_attempt
-from termcolor import colored
 from dotenv import load_dotenv
 from ast import literal_eval
 
 load_dotenv()
 
-# Runtime Environment's overprotective bouncer
+# Holler! Here's your key. Don't lose it.
 openai_api_key = os.getenv('OPENAI_API_KEY')
 
-# As per my understanding, this is NOT the name of a new GPT-4 model, but a cool way to refer to the current one
+# No secret password to the bat-cave, just plain old model name
 GPT_MODEL = "gpt-4-0613"
 
 @retry(wait=wait_random_exponential(min=1, max=40), stop=stop_after_attempt(3))
 def chat_completion_request(messages, functions=None, function_call=None, model=GPT_MODEL):
-    # Heads up! Incoming headers!
+    # Prepare for headers, folks!
     headers = {
         "Content-Type": "application/json",
         "Authorization": "Bearer " + openai_api_key,
     }
-    # Holy json-ly data, Batman!
+    # Presenting... json-ly data! (I promise it's not just corn syrup)
     json_data = {"model": model, "messages": messages, "max_tokens": 2048}
 
-    # If it's not None, it surely must be Something.
+    # If it's not None, it must mean something. Right?
     if functions is not None:
         json_data.update({"functions": functions})
     if function_call is not None:
@@ -43,32 +42,3 @@ def chat_completion_request(messages, functions=None, function_call=None, model=
         print("Unable to generate ChatCompletion response due to an unknown error.")
         print(f"Exception: {e}")
         raise
-
-
-def pretty_print_conversation(messages):
-    role_to_color = {
-        "system": "red",
-        "user": "green",
-        "assistant": "blue",
-        "function": "magenta",
-    }
-    formatted_messages = []
-    for message in messages:
-        if message["role"] == "system":
-            formatted_messages.append(f"system: {message['content']}\n")
-        elif message["role"] == "user":
-            formatted_messages.append(f"user: {message['content']}\n")
-        elif message["role"] == "assistant" and message.get("function_call"):
-            formatted_messages.append(f"assistant: {message['function_call']}\n")
-        elif message["role"] == "assistant" and not message.get("function_call"):
-            formatted_messages.append(f"assistant: {message['content']}\n")
-        elif message["role"] == "function":
-            formatted_messages.append(f"function ({message['name']}): {message['content']}\n")
-
-    for formatted_message in formatted_messages:
-        print(
-            colored(
-                formatted_message,
-                role_to_color[messages[formatted_messages.index(formatted_message)]["role"]],
-            )
-        )
