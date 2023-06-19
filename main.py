f76a96ced1d534f206e4bbd8074c8f0e7c54f6e3
Dylan Hart <god@linux.com>
2023-06-19 15:48:02 -0500

    nevermind, it's a flask api now

--- main.py
+++ main.py
@@ -1,19 +1,10 @@
-import argparse
-import os
-import requests
-from chat_utils import chat_completion_request, pretty_print_conversation
+from flask import Flask, request, jsonify
+from chat_utils import chat_completion_request
 from file_manager import FUNCTION_MAP
-from jsonpath_ng import parse
 from web_scraper import browse_web
+from ast import literal_eval
 
-# Parse command-line arguments
-parser = argparse.ArgumentParser(description="Run the chatbot with optional debug mode.")
-parser.add_argument("-d", "--debug", action="store_true", help="Enable debug mode")
-args = parser.parse_args()
-
-# Ensure the "projects" directory exists
-if not os.path.exists("projects"):
-    os.makedirs("projects")
+app = Flask(__name__)
 
 functions = [
     {
@@ -108,49 +99,32 @@ functions = [
     },
 ]
 
-messages = [
-    {"role": "system", "content": "You are a helpful and humorous assistant with software engineering skills, named Dynamo. You can create Python projects, generate multiple files according to Python standards, and populate them with content to form programs of various natures. You can also manage projects and files using the provided functions. When writing code, add a touch of humor in the comments to make the code more enjoyable to read. Never shorten code output when writing files."},
-]
+@app.route('/chat', methods=['POST'])
+def chat():
+    data = request.get_json()
+    if 'messages' not in data:
+        return jsonify({'error': 'Missing messages parameter'}), 400
+    messages = data['messages']
 
-while True:
-    print("User (type 'END' on a new line to finish input):")
-    user_input = ""
-    while True:
-        line = input()
-        if line == "END":
-            break
-        user_input += line + "\n"
-    messages.append({"role": "user", "content": user_input.strip()})
     while True:
         response = chat_completion_request(messages, functions=functions)
-        if args.debug:
-            print(f"Debug: Response: {response.text}")
-        try:
-            response_json = response.json()
-            assistant_message = parse('$.choices[0].message').find(response_json)
-            if not assistant_message:
-                assistant_message = {"role": "assistant", "content": "Sorry, I encountered an error while processing your request."}
-            else:
-                assistant_message = assistant_message[0].value
-        except Exception as e:
-            print(f"Error processing assistant's response: {e}")
-            assistant_message = {"role": "assistant", "content": "Sorry, I encountered an error while processing your request."}
-        messages.append(assistant_message)
+        response_json = response.json()
+        assistant_message = response_json['choices'][0]['message']
 
-        if assistant_message.get("function_call"):
-            function_name = assistant_message["function_call"]["name"]
-            function_arguments = assistant_message["function_call"]["arguments"]
-            function = FUNCTION_MAP[function_name]
-            try:
-                arguments = eval(function_arguments)
-            except Exception as e:
-                print(f'Error parsing function arguments: {e}')
-                break
-            try:
-                result = function(**arguments)
-                messages.append({"role": "function", "name": function_name, "content": result})
-            except Exception as e:
-                print(f'Error calling function {function_name}: {e}')
+        if assistant_message.get('function_call'):
+            function_name = assistant_message['function_call']['name']
+            function_arguments = assistant_message['function_call']['arguments']
+            function = FUNCTION_MAP[function_name] if function_name in FUNCTION_MAP else None
+
+            if not function:
+                return jsonify({'error': f'Function {function_name} not found'}), 404
+            arguments = literal_eval(function_arguments)
+            result = function(**arguments)
+            messages.append({'role': 'function', 'name': function_name, 'content': result})
         else:
+            messages.append(assistant_message)
             break
-    pretty_print_conversation(messages)
+    return jsonify({'messages': messages}), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
