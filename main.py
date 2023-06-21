fdd3fb90b0bb4cae7a686e540b88a5fcd0806b5d
Dylan Hart <god@linux.com>
2023-06-20 21:23:25 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from chat_utils import chat_completion_request
 from file_manager import FUNCTION_MAP
 from web_scraper import browse_web
 from ast import literal_eval
 
 app = Flask(__name__)
+CORS(app)
 
 functions = [
     {
@@ -89,14 +91,6 @@ functions = [
             "required": ["project_name"],
         },
     },
-    {
-        "name": "list_projects",
-        "description": "List all projects",
-        "parameters": {
-            "type": "object",
-            "properties": {},
-        },
-    },
 ]
 
 @app.route('/chat', methods=['POST'])
@@ -109,7 +103,15 @@ def chat():
     while True:
         response = chat_completion_request(messages, functions=functions)
         response_json = response.json()
-        assistant_message = response_json['choices'][0]['message']
+        try:
+            assistant_message = response_json['choices'][0]['message']
+        except KeyError as e:
+            print(response.text)
+            if response_json['error']:
+                error = response_json['error']['message']
+            else:
+                error = "unknown"
+            return jsonify({'openai-server-error': f'Error: {error}'}), 500
 
         if assistant_message.get('function_call'):
             function_name = assistant_message['function_call']['name']
@@ -127,4 +129,4 @@ def chat():
     return jsonify({'messages': messages}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, use_reloader=False)
