2d6f16a74ed281dce07a9a826a4700cd6cf5fe7f
Dylan Hart <god@linux.com>
2023-06-21 00:09:12 -0500

    add delete/rename

--- file_manager.py
+++ file_manager.py
@@ -9,6 +9,13 @@ def create_project(project_name):
     os.makedirs(project_path)
     return f"Successfully created project '{project_name}'"
 
+def create_subdir(project_name, dirname):
+    project_path = os.path.join("projects", project_name, dirname)
+    if os.path.exists(project_path):
+        return f"Error: Subdirectory '{dirname}' already exists in project '{project_name}'."
+    os.makedirs(project_path)
+    return f"Successfully created subdirectory '{dirname}' in project '{project_name}'"
+
 def list_projects():
     return "\n".join(os.listdir("projects"))
 
@@ -18,6 +25,16 @@ def list_files(project_name):
         return f"Error: Project '{project_name}' does not exist."
     files = os.listdir(project_path)
     return "\n".join(files)
+
+def delete_file(project_name, filename):
+    project_path = os.path.join("projects", project_name)
+    if not os.path.exists(project_path):
+        return f"Error: Project '{project_name}' does not exist."
+    file_path = os.path.join(project_path, filename)
+    if not os.path.exists(file_path):
+        return f"Error: File '{filename}' does not exist in project '{project_name}'."
+    os.remove(file_path)
+    return f"Successfully deleted {filename} in project '{project_name}'"
 
 def read_file(project_name, filename):
     project_path = os.path.join("projects", project_name)
@@ -34,15 +51,30 @@ def write_file(project_name, filename, content):
     project_path = os.path.join("projects", project_name)
     if not os.path.exists(project_path):
         return f"Error: Project '{project_name}' does not exist."
-    with open(os.path.join(project_path, filename), "w") as file:
+    file_path = os.path.join(project_path, filename)
+    with open(file_path, "w") as file:
         file.write(content)
     return f"Successfully wrote to {filename} in project '{project_name}'"
+
+def rename_file(project_name, old_filename, new_filename):
+    project_path = os.path.join("projects", project_name)
+    if not os.path.exists(project_path):
+        return f"Error: Project '{project_name}' does not exist."
+    old_file_path = os.path.join(project_path, old_filename)
+    if not os.path.exists(old_file_path):
+        return f"Error: File '{old_filename}' does not exist in project '{project_name}'."
+    new_file_path = os.path.join(project_path, new_filename)
+    os.rename(old_file_path, new_file_path)
+    return f"Successfully renamed {old_filename} to {new_filename} in project '{project_name}'"
 
 # Mapping function names to function objects
 FUNCTION_MAP = {
     'create_project': create_project,
+    'create_subdir': create_subdir,
     'list_files': list_files,
+    'delete_file': delete_file,
     'read_file': read_file,
     'write_file': write_file,
+    'rename_file': rename_file,
     'browse_web': browse_web
 }
